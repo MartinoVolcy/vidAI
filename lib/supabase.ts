b6c26de7e24b299a4537dae12beb2635f8c1e348
AustinFash <asf128@scarletmail.rutgers.edu>
2025-02-18 21:56:14 -0500

    Added Supabase Credentials

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = ''
-const supabaseAnonKey = ''
+const supabaseUrl = 'https://mkkmivwxkteziudydygu.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ra21pdnd4a3Rleml1ZHlkeWd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk3NjEyNDcsImV4cCI6MjA1NTMzNzI0N30.VLsnYReCfgZ6k_opDLASuZe3MfDbz3_8XwVVTOGZZ0M'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
